fb9ea08b0a4ab26361ccdcd15fef17d4bfb3e93f
Joe <ujbeszel@gmail.com>
2018-02-14 13:03:02 +0100

    Add disable_search_threshold and allow_single_deselect for chosen

--- exopite-simple-options/assets/dev/loader-jquery-chosen.js
+++ exopite-simple-options/assets/dev/loader-jquery-chosen.js
@@ -3,8 +3,12 @@
 
     $( document ).ready(function() {
 
-        $( '.exopite-sof-field-select ').find( 'select.chosen' ).chosen({
-            width: "300px"
+        $( '.exopite-sof-field-select ').find( 'select.chosen' ).each(function(index, el) {
+            $( el ).chosen({
+                disable_search_threshold: 15,
+                width: '300px',
+                allow_single_deselect: true
+            });
         });
 
     });
