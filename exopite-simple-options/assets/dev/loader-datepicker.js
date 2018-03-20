0f0b7f117c71ffc9f4053bb5de6dc6519b514452
Joe <ujbeszel@gmail.com>
2018-03-20 18:01:53 +0100

    Do not initialize date picker for clonable muster, avoid same ID

--- exopite-simple-options/assets/dev/loader-datepicker.js
+++ exopite-simple-options/assets/dev/loader-datepicker.js
@@ -4,6 +4,7 @@
     $( document ).ready(function() {
 
         $( '.datepicker' ).each(function(index, el) {
+            if ( $( el ).parents( '.exopite-sof-cloneable__muster' ).length ) return;
             var dateFormat = $( el ).data( 'format' );
             $( el ).datepicker( { 'dateFormat': dateFormat } );
         });
