27098d39866595230cc5214ad6a94f00db7e338d
Joe <ujbeszel@gmail.com>
2018-04-28 11:10:00 +0200

    In group display a textarea
    If editor is in group then display a textarea and call TinyMCE later on with JavaScript for multiple instance and drag and drop.

--- exopite-simple-options/fields/editor.php
+++ exopite-simple-options/fields/editor.php
@@ -14,21 +14,31 @@ if( ! class_exists( 'Exopite_Simple_Options_Framework_Field_editor' ) ) {
 
         public function output(){
 
-            $classes = ( isset( $this->field['class'] ) ) ? implode( ' ', explode( ' ', $this->field['class'] ) ) : '';
+            $classes = ( isset( $this->field['class'] ) ) ? explode( ' ', $this->field['class'] ) : array();//$this->element_class()
+            $classes[] = 'tinymce-js';
+            $classes = implode( ' ', $classes );
 
             echo $this->element_before();
 
-            $args = array(
-                'textarea_rows' => 15,
-                'editor_class' => $classes,
-                'textarea_name' => $this->element_name(),
-                'teeny' => false, // output the minimal editor config used in Press This
-                'dfw' => false, // replace the default fullscreen with DFW (supported on the front-end in WordPress 3.4)
-                'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
-                'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
-            );
+            if ( isset( $this->field['sub'] ) && $this->field['sub'] ) {
 
-            wp_editor( $this->element_value(), $this->field['id'], $args );
+                echo '<textarea id="' . $this->field['id'] . '" name="'. $this->element_name() .'" class="' . $classes . '"' . $this->element_attributes() .'>'. $this->element_value() .'</textarea>';
+
+            } else {
+
+                $args = array(
+                    'textarea_rows' => 15,
+                    'editor_class' => $classes,
+                    'textarea_name' => $this->element_name(),
+                    'teeny' => false, // output the minimal editor config used in Press This
+                    'dfw' => false, // replace the default fullscreen with DFW (supported on the front-end in WordPress 3.4)
+                    'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+                    'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
+                );
+
+                wp_editor( $this->element_value(), $this->field['id'], $args );
+
+            }
 
             echo $this->element_after();
 
