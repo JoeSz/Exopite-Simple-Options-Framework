ac43d922defd14a09cd18ba25be0547057fd8a61
Joe <ujbeszel@gmail.com>
2018-04-29 18:45:25 +0200

    Update version

--- exopite-simple-options/exopite-simple-options-framework-class.php
+++ exopite-simple-options/exopite-simple-options-framework-class.php
@@ -109,7 +109,7 @@ if ( ! class_exists( 'Exopite_Simple_Options_Framework' ) ) :
                 return;
             }
 
-            $this->version = '20180425';
+            $this->version = '20180429';
 
             // Filter for override
             $this->config  = apply_filters( 'exopite-simple-options-framework-config', $config );
