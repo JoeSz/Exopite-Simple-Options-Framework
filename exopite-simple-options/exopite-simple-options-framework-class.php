93dc017da319d89d7c38574d33c925e07ba782fc
Joe <ujbeszel@gmail.com>
2018-09-30 17:23:56 +0200

    Update version

--- exopite-simple-options/exopite-simple-options-framework-class.php
+++ exopite-simple-options/exopite-simple-options-framework-class.php
@@ -2,7 +2,7 @@
 	die;
 } // Cannot access pages directly.
 /**
- * Last edit: 2018-09-16
+ * Last edit: 2018-09-30
  *
  * INFOS AND TODOS:
  *
@@ -112,7 +112,7 @@ if ( ! class_exists( 'Exopite_Simple_Options_Framework' ) ) :
 
 		public $languages = array();
 
-		public $version = '20180924';
+		public $version = '20180930';
 
 		public $debug = false;
 
